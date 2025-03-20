65a0de6023f636150ca569f07f84aa8c73967527
esoriabonet <enzo.soriabonet.pro@gmail.com>
2025-03-20 14:45:52 +0100

    Création classe BoiteSegment

---
+++ module-de-gestion/eLightControl/boitesegment.h
@@ -0,0 +1,38 @@
+#ifndef BOITESEGMENT_H
+#define BOITESEGMENT_H
+
+#include <QtWidgets>
+
+/**
+ * @def ICONE_SEGMENT
+ * @brief L'icone de la lampe du segment
+ */
+#define ICONE_SEGMENT "lampe.png"
+
+/**
+ * @def LARGEUR_ICONE
+ * @brief La largeur de l'icone de la lampe
+ */
+#define LARGEUR_ICONE 100
+
+/**
+ * @def HAUTEUR_ICONE
+ * @brief La hauteur de l'icone de la lampe
+ */
+#define HAUTEUR_ICONE 100
+
+class BoiteSegment : public QWidget
+{
+    Q_OBJECT
+  public:
+    explicit BoiteSegment(int segmentId, QWidget* parent = nullptr);
+    void setConsommation(double nouvelleConsommation);
+
+  private:
+    int     idSegment;
+    QLabel* imageLabel;
+    QLabel* consommationLabel;
+    void    recupererDonnees();
+};
+
+#endif // BOITESEGMENT_H
