9c8f416ecdc792162be717e0ff6eefd36bbbeb54
esoriabonet <enzo.soriabonet.pro@gmail.com>
2025-03-27 14:06:41 +0100

    base de l'objet communicationBaseDeDonnes

--- module-de-gestion/eLightControl/communicationbasededonnees.cpp
+++ module-de-gestion/eLightControl/communicationbasededonnees.cpp
@@ -1,6 +1,42 @@
 #include "communicationbasededonnees.h"
 
-CommunicationBaseDeDonnees::CommunicationBaseDeDonnees(QObject *parent) : QObject(parent)
+CommunicationBaseDeDonnees::CommunicationBaseDeDonnees(QObject* parent) :
+    QObject(parent)
 {
+    baseDeDonnees = QSqlDatabase::addDatabase("QMYSQL");
+    baseDeDonnees.setHostName("localhost");
+    baseDeDonnees.setDatabaseName("eLight");
+    baseDeDonnees.setUserName("user_eLight");
+    baseDeDonnees.setPassword("lasalle84");
+}
 
+CommunicationBaseDeDonnees::~CommunicationBaseDeDonnees()
+{
+    deconnecter();
+}
+
+bool CommunicationBaseDeDonnees::connecter()
+{
+    if(!baseDeDonnees.open())
+    {
+        qDebug() << "Erreur connexion MySQL :"
+                 << baseDeDonnees.lastError().text();
+        return false;
+    }
+    qDebug() << "Connexion réussie à MySQL !";
+    return true;
+}
+
+void CommunicationBaseDeDonnees::deconnecter()
+{
+    if(baseDeDonnees.isOpen())
+    {
+        baseDeDonnees.close();
+        qDebug() << "Base de données MySQL déconnectée.";
+    }
+}
+
+bool CommunicationBaseDeDonnees::estConnecte() const
+{
+    return baseDeDonnees.isOpen();
 }
--- module-de-gestion/eLightControl/communicationbasededonnees.h
+++ module-de-gestion/eLightControl/communicationbasededonnees.h
@@ -2,15 +2,25 @@
 #define COMMUNICATIONBASEDEDONNEES_H
 
 #include <QObject>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QDebug>
 
 class CommunicationBaseDeDonnees : public QObject
 {
     Q_OBJECT
-public:
-    explicit CommunicationBaseDeDonnees(QObject *parent = nullptr);
+  public:
+    explicit CommunicationBaseDeDonnees(QObject* parent = nullptr);
+    ~CommunicationBaseDeDonnees();
+    bool connecter();
+    void deconnecter();
+    bool estConnecte() const;
 
-signals:
+  private:
+    QSqlDatabase baseDeDonnees;
 
+  signals:
 };
 
 #endif // COMMUNICATIONBASEDEDONNEES_H
