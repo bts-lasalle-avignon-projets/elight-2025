d62a2ed3555678fed2544e4d606a1e62e87b919c
esoriabonet <enzo.soriabonet.pro@gmail.com>
2025-03-31 09:58:34 +0200

    Récupération des valeurs du fichier config

--- module-de-gestion/eLightControl/communicationbasededonnees.cpp
+++ module-de-gestion/eLightControl/communicationbasededonnees.cpp
@@ -1,5 +1,8 @@
 #include "communicationbasededonnees.h"
 #include <QDebug>
+#include <QFile>
+#include <QSettings>
+#include <QCoreApplication>
 
 CommunicationBaseDeDonnees::CommunicationBaseDeDonnees(QObject* parent) :
     QObject(parent)
@@ -25,9 +28,28 @@ bool CommunicationBaseDeDonnees::connecter(
         qDebug() << Q_FUNC_INFO << "dataBaseName" << dataBaseName << "userName"
                  << userName << "password" << password << "hostName"
                  << hostName;
-        /**
-         * @todo mettre en place un fichier INI pour les paramètres de connexion
-         */
+
+        QString configPath = QCoreApplication::applicationDirPath() +
+                             "/config-base-de-donnees.ini";
+
+        if(QFile::exists(configPath))
+        {
+            qDebug() << Q_FUNC_INFO << "config-base-de-donnees.ini";
+
+            QSettings settings(configPath, QSettings::IniFormat);
+
+            hostName = settings.value("Database/host", hostName).toString();
+            dataBaseName =
+              settings.value("Database/name", dataBaseName).toString();
+            userName = settings.value("Database/user", userName).toString();
+            password = settings.value("Database/password", password).toString();
+        }
+        else
+        {
+            qWarning() << Q_FUNC_INFO
+                       << "Fichier config-base-de-donnees.ini non trouvé";
+        }
+
         baseDeDonnees.setHostName(hostName);
         baseDeDonnees.setDatabaseName(dataBaseName);
         baseDeDonnees.setUserName(userName);
